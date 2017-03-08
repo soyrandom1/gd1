091a5a2c5068ee038e9f92a639fddbc8487dafcb
nacho <ignacionaval52@gmail.com>
2017-03-08 14:57:45 -0300

    sistema rpg, no pude probar en esta pc

---
+++ generacion/script/player.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+export var speed = 140
+var raynode
+func _ready():
+	set_fixed_process(true)
+	raynode = get_node("RayCast2D")
+
+func _fixed_process(delta):
+	var move = vector2()
+	
+	if(Input.is_action_pressed("ui_up")):
+		move += Vector2(0,-1)
+		raynode.set_rotd(180)
+	if(Input.is_action_pressed("ui_down")):
+		move += Vector2(0,1)
+		raynode.set_rotd(0)
+	if(Input.is_action_pressed("ui_left")):
+		move += Vector2(-1,0)
+		raynode.set_rotd(-90)
+	if(Input.is_action_pressed("ui_right")):
+		move += Vector2(1,0)
+		raynode.set_rotd(90)
+				
+	
