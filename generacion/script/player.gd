a0b0f0f5c5585f075b4be20808ada95e194c8b73
nacho <ignacionaval52@gmail.com>
2017-03-11 09:18:19 -0300

    rotacion y nuevo sprite

--- generacion/script/player.gd
+++ generacion/script/player.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
-export var speedP = 140
+export var speedP = 200
 var raynode
 func _ready():
 	set_fixed_process(true)
@@ -17,9 +17,11 @@ func _fixed_process(delta):
 	if(Input.is_action_pressed("ui_left")):
 		move += Vector2(-1,0)
 		raynode.set_rotd(-90)
+		get_node( "Sprite" ).set_flip_h(true)
 	if(Input.is_action_pressed("ui_right")):
 		move += Vector2(1,0)
 		raynode.set_rotd(90)
+		get_node( "Sprite" ).set_flip_h(false)
 	move = move.normalized()*speedP*delta
 	move(move)
 	
