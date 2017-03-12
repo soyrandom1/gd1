6a8199fba234073f848d12e6c12f63dfb558d4f5
nacho <ignacionaval52@gmail.com>
2017-03-12 11:22:48 -0300

    -

--- generacion/script/player.gd
+++ generacion/script/player.gd
@@ -18,10 +18,15 @@ func _fixed_process(delta):
 		move += Vector2(-1,0)
 		raynode.set_rotd(-90)
 		get_node( "Sprite" ).set_flip_h(true)
+		get_node("CollisionPolygon2D").set_scale(Vector2(-1, 1))
+		get_node("CollisionPolygon2D").set_pos(get_node("CollisionPolygon2D").get_pos()-Vector2(3,0))
+
 	if(Input.is_action_pressed("ui_right")):
 		move += Vector2(1,0)
+		get_node("CollisionPolygon2D").set_pos(get_node("CollisionPolygon2D").get_pos()+Vector2(3,0))
 		raynode.set_rotd(90)
 		get_node( "Sprite" ).set_flip_h(false)
+		
 	move = move.normalized()*speedP*delta
 	move(move)
 	
