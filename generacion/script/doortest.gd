31358ff7f0470dbfe5ecf256e9d655410fe32355
nacho <ignacionaval52@gmail.com>
2017-03-29 22:22:33 -0300

    arreglado test de room, nuevo sprite heart

--- generacion/script/doortest.gd
+++ generacion/script/doortest.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+var t = false;
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
@@ -10,9 +10,12 @@ func _ready():
 
 
 func _on_Area2D_body_enter( body ):
-	if(body.get_name() == "pl"):
-		print("out")
-		var scene = preload("res://miniscenes/loadtest.tscn")
-		var node = scene.instance()
-		add_child(node)
-		node.set_pos(Vector2(-400,-614))
+	if(!t):
+		if(body.get_name() == "pl"):
+			print("out")
+			var scene = preload("res://miniscenes/loadtest.tscn")
+			var node = scene.instance()
+			add_child(node)
+			node.set_pos(Vector2(-400,-614))
+			t = true;
+		
